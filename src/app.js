e7bdf3f503298587776769a46fc0f7add93345fe
MCP <marc.castellap@mataro.epiaedu.cat>
2025-02-18 17:49:50 +0100

    Feat: Update view engine

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import express from "express";
 import { pool } from "./db.js";
 import { PORT } from "./config.js";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
+
+// View engine setup
+app.set('view engine', 'ejs');
+app.set('views', join(__dirname, '../views'));
 
 app.use(express.urlencoded({ extended: true }));
 
